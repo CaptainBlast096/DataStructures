765c3080bb0a63255bf4cc95f3206e2da6ddfe0a
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2023-01-25 17:08:21 -0500

    Create SinglyAPI.cpp

---
+++ SinglyAPI.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class API
+{
+    public:
+     PushFront();
+    TopFront();
+    PopFront();
+    PushBack();
+    TopBack();
+    
+}
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
