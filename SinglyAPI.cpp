b909b2caee42c12d7e8eeb6d05c9fb277a598f27
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2023-01-25 17:13:10 -0500

    Update SinglyAPI.cpp

--- SinglyAPI.cpp
+++ SinglyAPI.cpp
@@ -5,11 +5,18 @@ using namespace std;
 class API
 {
     public:
-     PushFront();
-    TopFront();
-    PopFront();
-    PushBack();
-    TopBack();
+    //Use template
+        PushFront(); // Takes one integer
+        TopFront(); // Takes one integer
+        PopFront(); //Takes one integer
+        PushBack();//Takes one integer
+        TopBack();//Takes one integer
+        PopBack();//Takes one integer
+        Find(); //Will be a boolean asking for an intege
+        Erase(); //Takes one integer
+        Empty(); // Boolean that asks for an integer
+        AddBefore(); // Takes one integer
+        AddAfter(); //Takes one integer
     
 }
 
